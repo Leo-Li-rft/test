7f204f045f29b1c1438fc5910a94d571e4efc02f
leoli2 <leoli2@refinitiv.com>
2020-07-29 14:18:22 +0800

    add some file

---
+++ task_run_tests.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -e
+
+# task script is in resource-tutorial/job-inputs/ folder
+# application input is in gopath/src/github.com/cloudfoundry-community/simple-go-web-app folder
+# $GOPATH is gopath/ folder
+echo 'Hello world'
